ad98bca855d987be71640e27e888858a48009f60
binhlengoc05 <lengochoangbinh123@gmail.com>
2025-05-05 05:42:37 +0700

    refactor graphics module

--- graphics.h
+++ graphics.h
@@ -1,6 +1,9 @@
-#ifndef _DEFS__H
-#define _DEFS__H
-#include<iostream>
+#ifndef _GRAPHICS__H
+#define _GRAPHICS__H
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include "defs.h"
 
 struct Graphics{
     SDL_Renderer *renderer;
@@ -13,13 +16,13 @@ struct Graphics{
     }
 
     //hàm khởi tạo
-    SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
+    void initSDL()
     {
         //hàm nếu không khởi tạo thành công thì trả vê báo lỗi
         if (SDL_Init(SDL_INIT_EVERYTHING) != 0)//=0 báo lỗi
             logErrorAndExit("SDL_Init", SDL_GetError());
     //
-        SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);//SDL_WINDOWPOS_CENTERED : trung tâm theo chiều dọc và ngang
+        window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);//SDL_WINDOWPOS_CENTERED : trung tâm theo chiều dọc và ngang
         //full screen
         //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
         if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
@@ -27,12 +30,12 @@ struct Graphics{
         if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
             logErrorAndExit( "SDL_image error:", IMG_GetError());
 
-        return window;
     }
 
-    SDL_Renderer* createRenderer(SDL_Window* window)
+
+    void createRenderer()
     {
-        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
                                                   SDL_RENDERER_PRESENTVSYNC);
         //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
         //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
@@ -42,10 +45,9 @@ struct Graphics{
         SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
         SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-        return renderer;
     }
 
-    void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+    void quitSDL()
     {
         IMG_Quit();
 
@@ -54,7 +56,19 @@ struct Graphics{
         SDL_Quit();
     }
 
-    void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer)
+    void prepareScene(SDL_Texture * background)
+    {
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy( renderer, background, NULL, NULL);
+    }
+
+    void presentScene()
+    {
+        SDL_RenderPresent(renderer);
+    }
+
+
+    void renderTexture(SDL_Texture *texture, int x, int y)
     {
         SDL_Rect dest;
 
@@ -65,7 +79,7 @@ struct Graphics{
         SDL_RenderCopy(renderer, texture, NULL, &dest);
     }
 
-    SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer)
+    SDL_Texture *loadTexture(const char *filename)
     {
         SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
 
@@ -74,6 +88,18 @@ struct Graphics{
             SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
 
         return texture;
+    }
+
+    void blitRect(SDL_Texture *texture, SDL_Rect *src, int x, int y)
+    {
+        SDL_Rect dest;
+
+        dest.x = x;
+        dest.y = y;
+        dest.w = src->w;
+        dest.h = src->h;
+
+        SDL_RenderCopy(renderer, texture, src, &dest);
     }
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -3,32 +3,34 @@
 #include <SDL_image.h>
 #include <SDL_mixer.h>
 #include "defs.h"
-//#include "graphics.h"
+#include "graphics.h"
 
 using namespace std;
 
-void logErrorAndExit(const char* msg, const char* error);
-SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE);
-SDL_Renderer* createRenderer(SDL_Window* window);
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+//void logErrorAndExit(const char* msg, const char* error);
+//SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE);
+//SDL_Renderer* createRenderer(SDL_Window* window);
+//void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
 void waitUntilKeyPressed();
-void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer);
-SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer);
+//void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer);
+//SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer);
 
 int main(int argc, char* argv[])
 {
     //cout << "Hello world!" << endl;
-    SDL_Window* window=initSDL(SCREEN_WIDTH,SCREEN_HEIGHT,WINDOW_TITLE);
-    SDL_Renderer* renderer = createRenderer(window);
+    Graphics graphics;
+    graphics.initSDL();
+    graphics.createRenderer();
 
-    SDL_Texture* background = loadTexture("background 2.jpg", renderer);
-    SDL_RenderCopy( renderer, background, NULL, NULL);
-
-    SDL_RenderPresent( renderer );
+    SDL_Texture* background = graphics.loadTexture("background 2.jpg");
+    graphics.prepareScene(background);
+    graphics.presentScene();
 
     waitUntilKeyPressed();
 
-    SDL_Texture* player= loadTexture("man run2.png", renderer);
+    SDL_Texture* player=graphics.loadTexture("man run2.png");
+    // cần sửa tiếp
+
     SDL_Rect playerRect;//khung hình chữ nhật của player
     SDL_QueryTexture(player, NULL, NULL, &playerRect.w, &playerRect.h);// cho playerRect lưu texture player theo chieu rong va cao vao playerRect.w/h
 
@@ -44,9 +46,9 @@ int main(int argc, char* argv[])
     playerRect.h=SCREEN_HEIGHT/11.0;
 
     //SDL_RenderCopy(renderer,player,NULL,&playerRect);// trường hợp lấy toàn bộ ảnh player
-    SDL_RenderCopy(renderer,player,&playerSrc,&playerRect);
+    SDL_RenderCopy(graphics.renderer,player,&playerSrc,&playerRect);
 
-    SDL_RenderPresent( renderer );
+    graphics.presentScene();
 
     waitUntilKeyPressed();
 
@@ -54,7 +56,8 @@ int main(int argc, char* argv[])
     background = NULL;
     SDL_DestroyTexture( player );
     player = NULL;
-    quitSDL(window,renderer);
+
+    graphics.quitSDL();
 
     return 0;
 }
@@ -65,59 +68,59 @@ int main(int argc, char* argv[])
 
 
 
-
-
-
-/* game loop với logic tối thiểu chỉ đủ chạy được chương trình */
-//hàm báo lỗi
-void logErrorAndExit(const char* msg, const char* error)
-{
-    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
-    SDL_Quit();
-}
-
-//hàm khởi tạo
-SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
-{
-    //hàm nếu không khởi tạo thành công thì trả vê báo lỗi
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)//=0 báo lỗi
-        logErrorAndExit("SDL_Init", SDL_GetError());
 //
-    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);//SDL_WINDOWPOS_CENTERED : trung tâm theo chiều dọc và ngang
-    //full screen
-    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
-
-    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
-        logErrorAndExit( "SDL_image error:", IMG_GetError());
-
-    return window;
-}
-
-SDL_Renderer* createRenderer(SDL_Window* window)
-{
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
-                                              SDL_RENDERER_PRESENTVSYNC);
-    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
-    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
-
-    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
-
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    return renderer;
-}
-
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
-{
-    IMG_Quit();
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
+//
+//
+///* game loop với logic tối thiểu chỉ đủ chạy được chương trình */
+////hàm báo lỗi
+//void logErrorAndExit(const char* msg, const char* error)
+//{
+//    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
+//    SDL_Quit();
+//}
+//
+////hàm khởi tạo
+//SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
+//{
+//    //hàm nếu không khởi tạo thành công thì trả vê báo lỗi
+//    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)//=0 báo lỗi
+//        logErrorAndExit("SDL_Init", SDL_GetError());
+////
+//    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);//SDL_WINDOWPOS_CENTERED : trung tâm theo chiều dọc và ngang
+//    //full screen
+//    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+//    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
+//
+//    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
+//        logErrorAndExit( "SDL_image error:", IMG_GetError());
+//
+//    return window;
+//}
+//
+//SDL_Renderer* createRenderer(SDL_Window* window)
+//{
+//    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+//                                              SDL_RENDERER_PRESENTVSYNC);
+//    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
+//    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+//
+//    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
+//
+//    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+//    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+//
+//    return renderer;
+//}
+//
+//void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+//{
+//    IMG_Quit();
+//
+//    SDL_DestroyRenderer(renderer);
+//    SDL_DestroyWindow(window);
+//    SDL_Quit();
+//}
+//
 void waitUntilKeyPressed()
 {
     SDL_Event e;
@@ -128,25 +131,25 @@ void waitUntilKeyPressed()
         SDL_Delay(100);
     }
 }
-
-void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer)
-{
-	SDL_Rect dest;
-
-	dest.x = x;
-	dest.y = y;
-	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
-
-	SDL_RenderCopy(renderer, texture, NULL, &dest);
-}
-
-SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer)
-{
-	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
-
-	SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
-	if (texture == NULL)
-        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
-
-	return texture;
-}
+//
+//void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer)
+//{
+//	SDL_Rect dest;
+//
+//	dest.x = x;
+//	dest.y = y;
+//	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+//
+//	SDL_RenderCopy(renderer, texture, NULL, &dest);
+//}
+//
+//SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer)
+//{
+//	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
+//
+//	SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
+//	if (texture == NULL)
+//        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
+//
+//	return texture;
+//}
